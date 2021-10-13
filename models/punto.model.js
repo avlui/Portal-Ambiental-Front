5fc2c688120dc26cf42cf29a5a9aed7ea15261f3
Giloc Delgado Cadavid <giloc.delgado@udea.edu.co>
2021-10-13 16:59:06 -0500

    autorregistro

--- models/punto.model.js
+++ models/punto.model.js
@@ -1,68 +1,73 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
-const puntoSchema = new mongoose.Schema(
-  {
+const puntoSchema = new mongoose.Schema({
+    nombre: {
+        type: String,
+        required: true
+    },
+    contrasena: {
+        type: String,
+        required: true
+    },
     nit: {
-      type: String,
-      required: true,
-      unique: true,
-      trim: true,
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
     },
     nombrePunto: {
-      type: String,
-      required: true,
-      trim: true,
+        type: String,
+        required: true,
+        trim: true
     },
     gerente: {
-      type: String,
-      required: true,
-      trim: true,
+        type: String,
+        required: true,
+        trim: true
     },
     ubicacion: {
-      type: {
-        latitud: Number,
-        longitud: Number,
-      },
-      required: true,
+        type: {
+            latitud: String,
+            longitud: String
+        },
+        required: true
     },
     direccion: {
-      type: String,
-      required: true,
+        type: String,
+        required: true
     },
     telefono: {
-      type: String,
-      required: true,
-      trim: true,
+        type: String,
+        required: true,
+        trim: true
     },
     email: {
-      type: String,
-      required: true,
-      trim: true,
+        type: String,
+        required: true,
+        trim: true
     },
     horario: {
-      type: String,
-      required: false,
-      trim: false,
+        type: String,
+        required: false,
+        trim: false
     },
     desperdicios: {
-      type: [mongoose.Schema.Types.ObjectId],
-      ref: "Desperdicio",
-      required: true,
+        type: [mongoose.Schema.Types.ObjectId],
+        ref: 'Desperdicio',
+        required: true
     },
     reportes: {
-      type: [mongoose.Schema.Types.ObjectId],
-      ref: "Reporte",
+        type: [mongoose.Schema.Types.ObjectId],
+        ref: 'Reporte',
     },
     estadisticas: {
-      type: [mongoose.Schema.Types.ObjectId],
-      ref: "Estadistica",
-    },
-  },
-  {
-    timestamps: true,
-  }
-);
+        type: [mongoose.Schema.Types.ObjectId],
+        ref: 'Estadistica'
+    }
+}, {
+    timestamps: true
+});
 
-const Punto = mongoose.model("Punto", puntoSchema);
+const Punto = mongoose.model('Punto', puntoSchema);
 
-module.exports = { Punto };
+module.exports = Punto;
