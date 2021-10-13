24cdb6a945446dd572cf68f0de8778488a44de88
Juan Pablo Yarce <juan.yarce1@udea.edu.co>
2021-10-13 15:23:32 -0500

    NavBar lista

--- src/Components/Layout/NavBar/NavBar.js
+++ src/Components/Layout/NavBar/NavBar.js
@@ -1,17 +1,26 @@
 import React from "react";
 import useUser from "../../Hooks/useUser";
+import "./NavBar.css";
+import logo from "./prosecto.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMapMarkedAlt } from "@fortawesome/free-solid-svg-icons";
+import { faSignInAlt } from "@fortawesome/free-solid-svg-icons";
+import { faSignOutAlt } from "@fortawesome/free-solid-svg-icons";
+import { faEdit } from "@fortawesome/free-solid-svg-icons";
 
 import { Link } from "react-router-dom";
 
 export default function Navigation() {
-
-  const {isLogged, logout} = useUser();
+  const { isLogged, logout } = useUser();
 
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+    <nav className="navbar navbar-expand-lg">
       <div className="container">
         <Link className="navbar-brand" to="/">
-          Dashboard
+          <div className="imageLogoContainer">
+            <img className="imageLogo" src={logo} alt="" />
+          </div>
+          <span className="logo"> Portal Ambiental</span>
         </Link>
         <button
           className="navbar-toggler"
@@ -28,28 +37,46 @@ export default function Navigation() {
           <ul className="navbar-nav ms-auto">
             <li className="nav-item ">
               <Link className="nav-link" to="/map">
-                Mapa
+                <FontAwesomeIcon
+                  icon={faMapMarkedAlt}
+                  size="2x"
+                  color="green"
+                ></FontAwesomeIcon>
               </Link>
             </li>
-            {
-              isLogged
-                ?
-                <li className="nav-item">
-                  <Link onClick={logout} className="nav-link" to="/">
-                    LogOut
-                  </Link>
-                </li>
-                :
-                <li className="nav-item">
-                  <Link className="nav-link" to="/login">
-                    LogIn
-                  </Link>
-                </li>
-            }
+            <li className="nav-item ">
+              <Link className="nav-link" to="/profile">
+                <FontAwesomeIcon
+                  icon={faEdit}
+                  size="2x"
+                  color="green"
+                ></FontAwesomeIcon>
+              </Link>
+            </li>
+            {isLogged ? (
+              <li className="nav-item">
+                <Link onClick={logout} className="nav-link" to="/">
+                  <FontAwesomeIcon
+                    icon={faSignOutAlt}
+                    size="2x"
+                    color="red"
+                  ></FontAwesomeIcon>
+                </Link>
+              </li>
+            ) : (
+              <li className="nav-item">
+                <Link className="nav-link" to="/login">
+                  <FontAwesomeIcon
+                    icon={faSignInAlt}
+                    size="2x"
+                    color="green"
+                  ></FontAwesomeIcon>
+                </Link>
+              </li>
+            )}
           </ul>
         </div>
       </div>
     </nav>
   );
-
 }
